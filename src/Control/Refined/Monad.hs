28268b5508e6a5a3993b007a43d65acda012989f
Henry Blanchette <henry@monkeyface.com>
2021-03-10 10:59:27 -0500

    array properties outline

--- src/Control/Refined/Monad.hs
+++ src/Control/Refined/Monad.hs
@@ -16,10 +16,16 @@ import Prelude hiding (Monad, pure, seq)
 {-@
 data Monad m = Monad
   { pure :: forall a. a -> m a,
-    bind :: forall a b. m a -> (a -> m b) -> m b
-  }
-@-}
-{-
+    bind :: forall a b. m a -> (a -> m b) -> m b,
+    kleisli :: forall a b c. (a -> m b) -> (b -> m c) -> (a -> m c),
+    kleisli_correct ::
+      forall a b c.
+      k1:(a -> m b) ->
+      k2:(b -> m c) ->
+      x:a ->
+      EqualProp (m c)
+        {kleisli k1 k2 x}
+        {bind (k1 x) k2},
     bind_identity_left ::
       forall a b.
       x:a ->
@@ -27,45 +33,52 @@ data Monad m = Monad
       EqualProp (m b)
         {bind (pure x) k}
         {k x},
--}
+    bind_identity_right ::
+      forall a.
+      m:m a ->
+      EqualProp (m a)
+        {bind m pure}
+        {m},
+    bind_associativity ::
+      forall a b c.
+      m:m a ->
+      k1:(a -> m b) ->
+      k2:(b -> m c) ->
+      EqualProp (m c)
+        {bind (bind m k1) k2}
+        {bind m (kleisli k1 k2)}
+  }
+@-}
 data Monad m = Monad
   { pure :: forall a. a -> m a,
-    bind :: forall a b. m a -> (a -> m b) -> m b
+    bind :: forall a b. m a -> (a -> m b) -> m b,
+    kleisli :: forall a b c. (a -> m b) -> (b -> m c) -> (a -> m c),
+    kleisli_correct ::
+      forall a b c.
+      (a -> m b) ->
+      (b -> m c) ->
+      a ->
+      EqualityProp (m c),
+    bind_identity_left ::
+      forall a b.
+      a ->
+      (a -> m b) ->
+      EqualityProp (m b),
+    bind_identity_right ::
+      forall a.
+      m a ->
+      EqualityProp (m a),
+    bind_associativity ::
+      forall a b c.
+      m a ->
+      (a -> m b) ->
+      (b -> m c) ->
+      EqualityProp (m c)
   }
-
-{-@
-class MonadLaws m where
-  bind_identity_left :: mnd:Monad m -> x:a -> k:(a -> m b) -> (EqualProp (m b) {bind mnd (pure mnd x) k} {k x})
-  bind_identity_right :: mnd:Monad m -> m:m a -> (EqualProp (m a) {bind mnd m (pure mnd)} {m})
-  bind_associativity :: mnd:Monad m -> m:m a -> k1:(a -> m b) -> k2:(b -> m c) -> (EqualProp (m c) {bind mnd (bind mnd m k1) k2} {bind mnd m (\x:a -> bind mnd (k1 x) k2)})
-@-}
-class MonadLaws m where
-  bind_identity_left ::
-    forall a b.
-    Monad m ->
-    a ->
-    (a -> m b) ->
-    EqualityProp (m b)
-  bind_identity_right ::
-    forall a.
-    Monad m ->
-    m a ->
-    EqualityProp (m a)
-  bind_associativity ::
-    forall a b c.
-    Monad m ->
-    m a ->
-    (a -> m b) ->
-    (b -> m c) ->
-    EqualityProp (m c)
 
 {-
 ## Utilities
 -}
-
-{-@ reflect kleisli @-}
-kleisli :: Monad m -> (a -> m b) -> (b -> m c) -> (a -> m c)
-kleisli mnd k1 k2 x = bind mnd (k1 x) k2
 
 {-@ reflect join @-}
 join :: Monad m -> m (m a) -> m a
@@ -82,3 +95,24 @@ map mnd f m = bind mnd m (\x -> pure mnd (f x))
 {-@ reflect map2 @-}
 map2 :: Monad m -> (a -> b -> c) -> (m a -> m b -> m c)
 map2 mnd f ma mb = bind mnd ma (\x -> bind mnd mb (\y -> pure mnd (f x y)))
+
+{-
+## Properties
+-}
+{-@
+seq_associativity ::
+  mnd:Monad m ->
+  ma:m a ->
+  mb:m b ->
+  mc:m c ->
+  EqualProp (m c)
+    {seq mnd (seq mnd ma mb) mc}
+    {seq mnd ma (seq mnd mb mc)}
+@-}
+seq_associativity ::
+  Monad m ->
+  m a ->
+  m b ->
+  m c ->
+  EqualityProp (m c)
+seq_associativity mnd ma mb mc = undefined -- TODO
--- src/Control/Refined/Monad/Array.hs
+++ src/Control/Refined/Monad/Array.hs
@@ -121,9 +121,7 @@ data Array m a = Array
 ## Utilities
 -}
 
-{-@
-readList :: Array m a -> Index -> n:Natural -> m (List a)
-@-}
+{-@ reflect readList @-}
 readList :: Array m a -> Index -> Natural -> m (List a)
 readList ary i Z = pure mnd Nil
   where
@@ -132,30 +130,24 @@ readList ary i (S n) = map2 mnd Cons (read ary i) (readList ary (S i) n)
   where
     mnd = monad ary
 
-{-@
-writeList :: Array m a -> Index -> List a -> m Unit
-@-}
 {-@ reflect writeList @-}
 writeList :: Array m a -> Index -> List a -> m Unit
 writeList ary i Nil = pure mnd it
   where
     mnd = monad ary
-writeList ary i (Cons x xs) = seq mnd (write ary i x) (writeList ary (S i) xs)
+writeList ary i (Cons x xs) = write ary i x >> writeList ary (S i) xs
   where
+    (>>) = seq mnd
     mnd = monad ary
 
-{-@
-writeListToLength :: Array m a -> Index -> List a -> m Natural
-@-}
+{-@ reflect writeListToLength @-}
 writeListToLength :: Array m a -> Index -> List a -> m Natural
 writeListToLength ary i xs = seq mnd (writeList ary i xs) (pure mnd (length xs))
   where
+    (>>) = seq mnd
     mnd = monad ary
 
-{-@
-writeListToLength2 ::
-  Array m a -> Index -> (List a, List a) -> m (Natural, Natural)
-@-}
+{-@ reflect writeListToLength2 @-}
 writeListToLength2 ::
   Array m a -> Index -> (List a, List a) -> m (Natural, Natural)
 writeListToLength2 ary i (xs, ys) =
@@ -165,10 +157,7 @@ writeListToLength2 ary i (xs, ys) =
   where
     mnd = monad ary
 
-{-@
-writeListToLength3 ::
-  Array m a -> Index -> (List a, List a, List a) -> m (Natural, Natural, Natural)
-@-}
+{-@ reflect writeListToLength3 @-}
 writeListToLength3 ::
   Array m a -> Index -> (List a, List a, List a) -> m (Natural, Natural, Natural)
 writeListToLength3 ary i (xs, ys, zs) =
@@ -204,86 +193,111 @@ writeList_append ::
   EqualityProp (m Unit)
 --
 writeList_append ary i Nil ys =
-  let t1 = writeList_ i (Nil ++ ys)
-      t2 = writeList_ i ys -- append_identity
-      t3 = (\_ -> writeList_ i ys) it -- betaEquivalencyTrivial
-      t4 = pure_ it >>= (\_ -> writeList_ i ys) -- bind_identity_left
-      t5 = pure_ it >> writeList_ i ys -- def >>
-      t6 = writeList_ i Nil >> writeList_ i ys -- def writeList
-      t7 = writeList_ i Nil >> writeList_ (i `add` Z) ys -- add_identity i
-      t8 = writeList_ i Nil >> writeList_ (i `add` length Nil) ys -- def length
+  let --
+      -- steps
+      --
+      t1 = writeList ary i (Nil ++ ys)
+      -- append_identity
+      t2 = writeList ary i ys
+      -- betaEquivalencyTrivial
+      t3 = (\_ -> writeList ary i ys) it
+      -- bind_identity_left
+      t4 = pure mnd it >>= (\_ -> writeList ary i ys)
+      -- >>
+      t5 = pure mnd it >> writeList ary i ys
+      -- writeList
+      t6 = writeList ary i Nil >> writeList ary i ys
+      -- add_identity i
+      t7 = writeList ary i Nil >> writeList ary (i `add` Z) ys
+      -- length
+      t8 = writeList ary i Nil >> writeList ary (i `add` length Nil) ys
+      --
+      -- proofs
       --
       ep_t1_t2 =
-        (substitutability (writeList_ i) (Nil ++ ys) ys) -- writeList_ i (...)
+        (substitutability (writeList ary i) (Nil ++ ys) ys) -- writeList_ i (...)
           ( (fromSMT (Nil ++ ys) ys)
               (append_identity ys) -- Nil ++ ys = ys
           )
-          ? writeList_ i (Nil ++ ys)
-          ? writeList_ i ys
+          ? writeList ary i (Nil ++ ys)
+          ? writeList ary i ys
       ep_t2_t3 =
-        betaEquivalencyTrivial it (writeList_ i ys)
-          ? (\_ -> writeList_ i ys) it
+        betaEquivalencyTrivial it (writeList ary i ys)
+          ? (\_ -> writeList ary i ys) it
       ep_t3_t4 =
-        -- bind_identity_left mnd it (\_ -> writeList_ i ys)
-        --   ? (pure_ it >>= (\_ -> writeList_ i ys))
-        --   ? (\_ -> writeList_ i ys) it
-        undefined
+        -- symmetry t4 t3 $
+        --   bind_identity_left mnd it (\_ -> writeList_ i ys)
+        undefined -- TODO
       ep_t4_t5 =
-        undefined
+        fromSMT t4 t5 trivial
       ep_t5_t6 =
-        undefined
+        fromSMT t5 t6 trivial
       ep_t6_t7 =
-        undefined
+        fromSMT t6 t7 $ add_identity i
       ep_t7_t8 =
-        undefined
+        fromSMT t7 t8 trivial
    in --
-      (transitivity t1 t4 t8)
-        ( (transitivity t1 t2 t4)
-            ep_t1_t2
-            ( (transitivity t2 t3 t4)
-                ep_t2_t3
-                ep_t3_t4
-            )
-        )
-        ( (transitivity t4 t6 t8)
-            ( (transitivity t4 t5 t6)
-                ep_t4_t5
-                ep_t5_t6
-            )
-            ( (transitivity t6 t7 t8)
-                ep_t6_t7
-                ep_t7_t8
-            )
-        )
+      -- structure
+      --
+      transitivity t1 t2 t8 ep_t1_t2 $
+        transitivity t2 t3 t8 ep_t2_t3 $
+          transitivity t3 t4 t8 ep_t3_t4 $
+            transitivity t4 t5 t8 ep_t4_t5 $
+              transitivity t5 t6 t8 ep_t5_t6 $
+                transitivity t6 t7 t8 ep_t6_t7 ep_t7_t8
   where
-    writeList_ = writeList ary
     (>>) = seq mnd
     (>>=) = bind mnd
-    pure_ = pure mnd
     mnd = monad ary
 --
-writeList_append ary i (Cons x xs) ys = undefined
-
--- {-@
--- test ::
---   ary:Array m a ->
---   i:Index ->
---   xs:List a ->
---   EqualProp (m Unit)
---     {writeList ary i (append Nil xs)}
---     {writeList ary i xs}
--- @-}
--- test :: Array m a -> Index -> List a -> EqualityProp (m Unit)
--- test ary i xs =
---   let t1 = writeList_ i (Nil ++ xs)
---       t2 = writeList_ i xs -- append_identity
---    in (substitutability (writeList_ i) (Nil ++ xs) xs) -- writeList_ i (...)
---         (fromSMT (Nil ++ xs) xs (append_identity xs)) -- Nil ++ xs = xs
---         ? writeList_ i (Nil ++ xs)
---         ? writeList_ i xs
---   where
---     writeList_ = writeList ary
---     (>>) = seq mnd
---     (>>=) = bind mnd
---     pure_ = pure mnd
---     mnd = monad ary
+writeList_append ary i (Cons x xs) ys =
+  let --
+      -- steps
+      --
+      t1 = writeList ary i (Cons x xs ++ ys)
+      -- Cons x xs ++ ys
+      t2 = writeList ary i (Cons x (xs ++ ys))
+      -- writeList ...
+      t3 = write ary i x >> writeList ary (S i) (xs ++ ys)
+      -- writeList_append ary (S i) xs ys
+      t4 = write ary i x >> (writeList ary (S i) xs >> writeList ary (S i `add` length xs) ys)
+      -- S i `add` length xs
+      t5 = write ary i x >> (writeList ary (S i) xs >> writeList ary (S (i `add` length xs)) ys)
+      -- symmetry $ seq_associativity (write ary i x) (writeList ary (S i) xs) (writeList ary (S (i `add` length xs)) ys)
+      t6 = (write ary i x >> writeList ary (S i) xs) >> writeList ary (S (i `add` length xs)) ys
+      -- symmetry $ add_S_right i (lenght xs)
+      t7 = (write ary i x >> writeList ary (S i) xs) >> writeList ary (i `add` S (length xs)) ys
+      -- symmetry $ length (Cons x xs)
+      t8 = (write ary i x >> writeList ary (S i) xs) >> writeList ary (i `add` length (Cons x xs)) ys
+      -- symmetry $ writeList ary i (Cons x xs)
+      t9 = writeList ary i (Cons x xs) >> writeList ary (i `add` length (Cons x xs)) ys
+      --
+      -- proofs
+      --
+      ep_t1_t2 =
+        substitutability (writeList ary i) (Cons x xs ++ ys) (Cons x (xs ++ ys)) $
+          fromSMT (Cons x xs ++ ys) (Cons x (xs ++ ys)) trivial
+            ? writeList ary i (Cons x xs ++ ys)
+            ? writeList ary i (Cons x (xs ++ ys))
+            ? xs ++ ys
+      ep_t2_t3 = undefined
+      ep_t3_t4 = undefined
+      ep_t4_t5 = undefined
+      ep_t5_t6 = undefined
+      ep_t6_t7 = undefined
+      ep_t7_t8 = undefined
+      ep_t8_t9 = undefined
+   in --
+      -- structure
+      --
+      transitivity t1 t2 t9 ep_t1_t2 $
+        transitivity t2 t3 t9 ep_t2_t3 $
+          transitivity t3 t4 t9 ep_t3_t4 $
+            transitivity t4 t5 t9 ep_t4_t5 $
+              transitivity t5 t6 t9 ep_t5_t6 $
+                transitivity t6 t7 t9 ep_t6_t7 $
+                  transitivity t7 t8 t9 ep_t7_t8 ep_t8_t9
+  where
+    (>>) = seq mnd
+    (>>=) = bind mnd
+    mnd = monad ary
